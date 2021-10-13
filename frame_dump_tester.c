643848f8cfbac6ae7f109cef614776002a65e625
Andrew Stitcher <astitcher@apache.org>
2021-10-13 13:51:58 -0400

    Added commands to allow test printing raw bytes
    Special commands start the line with \
    Commands are \b for quoted string treated as bytes, useful to
    cut-n-paste raw trace logging; \x or \h for hex that gets treated as raw
    bytes.

--- frame_dump_tester.c
+++ frame_dump_tester.c
@@ -8,7 +8,7 @@
 
 extern int pn_data_parse(pn_data_t* data, size_t len, const char* s);
 extern int pn_data_parse_string(pn_data_t* data, const char* s);
-extern void pn_value_dump(pn_bytes_t frame, pn_string_t *output);
+extern size_t pn_value_dump(pn_bytes_t frame, pn_string_t *output);
 
 void hexdump(size_t size, const char* buffer)
 {
@@ -21,26 +21,72 @@ void hexdump(size_t size, const char* buffer)
 
 void process(pn_data_t* data, const char* str)
 {
+    bool binary = false;
+    bool hex = false;
     pn_data_clear(data);
     pn_error_clear(pn_data_error(data));
 
-    int r = pn_data_parse_string(data, str);
+    // Check for special commands
+    if (*str=='\\'){
+        ++str;
+        switch (*str) {
+          case 'b':
+            // Binary
+            binary = true;
+            break;
+          case 'x':
+          case 'h':
+            hex = true;
+            break;
+          default:
+            printf("Unrecognized special command: %c\n", *str);
+            return;
+        }
+        ++str;
+    }
 
-    if (r==0) {
+    char buffer[1024];
+    ssize_t s = 0;
+    if (hex) {
+      char hex[3] = {0,0,0};
+      uint8_t hc = 0;
+      for (const char* c=str; *c; ++c) {
+        if (isspace(*c)) continue;
+        if (!isxdigit(*c)) {
+          printf("Illegal hex char: %c\n", *c);
+          return;
+        }
+        hex[hc++] = *c;
+        if (hc==2) {
+          buffer[s++] = strtoul(hex, NULL, 16);
+          hc = 0;
+        }
+      }
+    } else {
+        int r = pn_data_parse_string(data, str);
+        if (r!=0) {
+          printf("Failed: %s\n", pn_error_text(pn_data_error(data)));
+          return;
+        }
         pn_data_print(data);
         printf("\n");
 
-        char buffer[1024];
-        ssize_t s = pn_data_encode(data, buffer, sizeof(buffer));
-        printf("Encoded: %zd bytes:\n", s);
-        hexdump(s, buffer);
-
-        pn_string_t* out = pn_string("");
-        pn_value_dump((pn_bytes_t){.size=s,.start=buffer}, out);
-        printf("%s\n", pn_string_get(out));
-    } else {
-        printf("Failed: %s\n", pn_error_text(pn_data_error(data)));
+        if (binary) {
+            // In this mode we assume the single value is a binary item that is the bytes to be decoded
+            pn_bytes_t bytes = pn_data_get_bytes(data);
+            s = bytes.size;
+            memcpy(buffer, bytes.start, s);
+        } else {
+            s = pn_data_encode(data, buffer, sizeof(buffer));
+        }
     }
+    printf("Encoded: %zd bytes:\n", s);
+    hexdump(s, buffer);
+
+    pn_string_t* out = pn_string("");
+    size_t ds = pn_value_dump((pn_bytes_t){.size=s,.start=buffer}, out);
+    printf("%s\n", pn_string_get(out));
+    printf("(Decoded %zd bytes)\n", ds);
 }
 
 int main(int argc, const char* argv[])
